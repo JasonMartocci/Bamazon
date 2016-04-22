968e18262172833e388c370942de983516c5a122
Jason Martocci <martoccijason@gmail.com>
2016-04-22 10:20:24 -0400

    connected to sql db

---
+++ mySql_scripts.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Products (ItemID int AUTO_INCREMENT, ProductName varchar(255), DepartmentName varchar(255), Price int, StockQuantity int, PRIMARY KEY (ItemID));
+
+
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ('Crest', 'Health Care', 2.00, 34);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ('Total Cereal', 'Groceries', 4.00, 10);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ('Scope', 'Health Care', 4.40, 23);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ('Raisin Bran', 'Groceries', 3.00, 34);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ('Dental Floss', 'Health Care', 1.00, 44);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ('Salmon', 'Seafood', 18.00, 34);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ('Capt Crunch', 'Groceries', 4.20, 18);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ('Tuna', 'Seafood', 12.00, 22);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ('Crabs', 'Seafood', 13.00, 31);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ('Shrimp', 'Seafood', 16.00, 11);
