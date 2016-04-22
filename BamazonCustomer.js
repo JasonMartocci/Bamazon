b45f55f18ae08b60d369ee4a182a35d0cb168f1c
Jason Martocci <martoccijason@gmail.com>
2016-04-22 13:00:01 -0400

    got quantity working

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -16,17 +16,27 @@ connection.connect(function(err) {
 });
 
 connection.query("SELECT * FROM Bamazon.Products", function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log("Welcome to Bamazon!");
-  for(var i=0;i<rows.length;i++){
-    console.log(("ItemId: " + rows[i].ItemID) + " " + rows[i].ProductName + " || $" + rows[i].Price.toFixed(2) + " (" + rows[i].StockQuantity + " available)");
-  }
+	if (err) throw err;
+	console.log("Welcome to Bamazon!");
+	for(var i=0;i<rows.length;i++){
+		console.log(("ItemId: " + rows[i].ItemID) + " " + rows[i].ProductName + " | $" + rows[i].Price.toFixed(2) + " | " + rows[i].StockQuantity + " available");
+	}
+	prompt.get(['ItemID', 'StockQuantity'], function (err, result, ItemID) {
+		for(var j=0;j<rows.length;j++){
+			if (result.StockQuantity <= rows[j].StockQuantity){
+				console.log(rows[j].StockQuantity + " they match");
+			}else{
+				console.log(rows[j].StockQuantity + " Insufficient quantity");
+			};
+		};
+		console.log("\n");
+		console.log('  Product ItemId: ' + result.ItemID);
+		console.log('  Product Quanity: ' + result.StockQuantity);
+	});
 });
 
-prompt.get(['ItemID', 'StockQuantity'], function (err, result) {
-	console.log('  Product ItemId: ' + result.ItemID);
-	console.log('  Product Quanity: ' + result.StockQuantity);
-});
+// function test_prompt(){
+// }
+// test_prompt();
 
 connection.end();
